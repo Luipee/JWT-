5d202654fb2c077e79662935dda8eef99e8d2d8e
Luipee <lipeffcampos@gmail.com>
2023-01-23 17:03:03 -0300

    finalizado

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ app.use(express.json())
 app.get('/', (req, res) => {
   res.status(200).json({ message: 'Bem vindo a nossa   api' })
 })
-app.get('/user/id', async (req, res) => {
+app.get('/user/:id', checkToken, async (req, res) => {
   const id = req.params.id
 
   const user = await User.findById(id, '-password')
@@ -23,6 +23,22 @@ app.get('/user/id', async (req, res) => {
 
   res.status(200).json({ user })
 })
+
+function checkToken (req, res, next) {
+  const authHeader = req.headers.authorization
+  const token = authHeader && authHeader.split('')[1]
+
+  if (!token) {
+    return res.status(401).json({ message: 'acesso negado!' })
+  }
+  try {
+    const secret = process.env.SECRET
+    jwt.verify(token, secret)
+    next()
+  } catch (error) {
+    res.status(400).json({ message: 'token Invalido' })
+  }
+}
 app.post('/auth/register', async (req, res) => {
   const { name, email, password, confirmpassword } = req.body
 
